088f0c744d94d3f795dfe60a203a37d5c898d1f0
Justin <robojoos@gmail.com>
2018-03-02 14:53:57 -0800

    some pointcloud methods generating

---
+++ src/PclSharp/Struct/PointTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PclSharp.Struct
+{
+    [StructLayout(LayoutKind.Sequential, Size =16)]
+    public struct PointXYZRGBA
+    {
+        public float X, Y, Z;
+        public uint RGBA;
+    }
+}
