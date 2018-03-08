9012375fb62f72701a4572b56828fa31c2389c06
Justin <robojoos@gmail.com>
2018-03-08 12:17:25 -0800

    working on filters, pcdreader

---
+++ src/PclSharp/Filters/Filter.cs
@@ -0,0 +1,13 @@
+﻿using PclSharp.Std;
+using System;
+using System.Collections.Generic;
+
+namespace PclSharp.Filters
+{
+    public abstract class Filter<PointT> : UnmanagedObject
+    {
+        public abstract void filter(PointCloud<PointT> output);
+        public abstract void SetInputCloud(PointCloud<PointT> cloud);
+        public abstract VectorOfInt Indices { get; set; }
+    }
+}
