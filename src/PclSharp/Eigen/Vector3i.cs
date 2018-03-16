c1cac78a09aa068443fbe5a9ffb00a7378b7821b
Justin <robojoos@gmail.com>
2018-03-16 14:18:29 -0700

    properties for voxelgrid

---
+++ src/PclSharp/Eigen/Vector3i.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PclSharp.Eigen
+{
+    [DebuggerDisplay("{X}, {Y}, {Z}")]
+    [StructLayout(LayoutKind.Explicit, Size = 16)]
+    public unsafe struct Vector3i
+    {
+        [FieldOffset(0)]
+        public int X;
+        [FieldOffset(4)]
+        public int Y;
+        [FieldOffset(8)]
+        public int Z;
+
+        [FieldOffset(0)]
+        public fixed int data[4];
+    }
+}
--- src/PclSharp/Filters/VoxelGrid.cs
+++ src/PclSharp/Filters/VoxelGrid.cs
@@ -5,5 +5,11 @@ namespace PclSharp.Filters
 	{
 		public abstract (double min, double max) FilterLimits { get; set; }
 		public abstract string FilterFieldName { get; set; }
+		public abstract bool DownsampleAllData { get; set; }
+		public abstract int MinimumPointsNumberPerVoxel { get; set; }
+		public abstract Eigen.Vector3i MinBoxCoordinates { get; }
+		public abstract Eigen.Vector3i MaxBoxCoordinates { get; }
+		public abstract Eigen.Vector3i NrDivisions { get; }
+		public abstract Eigen.Vector3i DivisionMultiplier { get; }
 	}
 }
