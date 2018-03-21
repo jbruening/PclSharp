6a043464ec0c573074bbd6734fe2003768fbb9b5
Justin <robojoos@gmail.com>
2018-03-21 16:35:33 -0700

    begin pcl::surface

---
+++ src/PclSharp/Surface/SurfaceBase.cs
@@ -0,0 +1,12 @@
+﻿using PclSharp.Search;
+using System;
+using System.Collections.Generic;
+
+namespace PclSharp.Surface
+{
+    public abstract class SurfaceBase<PointT> : PclBase<PointT>
+    {
+        public abstract void SetSearchMethod(Search<PointT> search);
+        public abstract void Reconstruct(PolygonMesh output);
+    }
+}
